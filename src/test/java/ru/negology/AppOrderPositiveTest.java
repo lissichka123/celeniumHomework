fa9fce8419af0bfb5f356cea9334ad8c06733bcb
Ekaterina Turkina <katrina.turkina@gmail.com>
2024-04-21 18:37:52 +0300

    changes tests 2

--- src/test/java/ru/negology/AppOrderPositiveTest.java
+++ src/test/java/ru/negology/AppOrderPositiveTest.java
@@ -1,17 +1,11 @@
 package ru.negology;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
